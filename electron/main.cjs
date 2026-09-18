9d982f39e7b5ab98733e201f2f05c1fac16cdbed
fenixlife1978 <generaextra@gmail.com>
2026-09-18 13:21:38 -0400

    fix: usar ICO oficial en la ventana Electron

--- electron/main.cjs
+++ electron/main.cjs
@@ -17,7 +17,7 @@ function createWindow() {
       preload: path.join(__dirname, 'preload.cjs'),
       webSecurity: true,
     },
-    icon: path.join(app.getAppPath(), 'build/icons/TiendaPOS.ico'),
+    icon: path.join(app.getAppPath(), 'build/icons/TiendaPOS-256.ico'),
     show: false,
   });
 
